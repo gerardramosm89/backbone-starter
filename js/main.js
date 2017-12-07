4599341c76303dfca50a27fa1fee6c23f4392f8e
Gerry <gerard.ramosm89@gmail.com>
2017-12-06 21:15:30 -0800

    test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-var Song = Backbone.Model.extend({
-    initialize: function() {
-        console.log('new song created!');
-    }
-});
+// Creating Models
+var Album = Backbone.Model.extend();
+var album = new Album();
+album.set('title', 'This is the title');
 
+// Validating
+var Song = Backbone.Model.extend({
+    validate: function(attrs) {
+        if(!attrs.title) return 'title is required';
+    }
+})
 var song = new Song();
